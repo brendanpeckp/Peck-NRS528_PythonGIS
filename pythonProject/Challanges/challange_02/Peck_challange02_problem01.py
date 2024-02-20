dcf5779b0328066d979ccddddc33fb468afb6ee1
brendanpeckp <157844873+brendanpeckp@users.noreply.github.com>
2024-02-20 14:52:17 -0500

    Challange02_Problem01_Edits

--- pythonProject/Challanges/challange_02/Peck_challange02_problem01.py
+++ pythonProject/Challanges/challange_02/Peck_challange02_problem01.py
@@ -6,51 +6,15 @@
 ## [1, 2, 3, 6, 8, 12, 20, 32, 46, 85]
 ## ```
 List1 = [1, 2, 3, 6, 8, 12, 20, 32, 46, 85]
-for i in List1:
-    if i == 6:
-        List1.remove(i)
-for i in List1:
-    if i == 8:
-        List1.remove(i)
-for i in List1:
-    if i == 12:
-        List1.remove(i)
-for i in List1:
-    if i == 20:
-        List1.remove(i)
-for i in List1:
-    if i == 32:
-        List1.remove(i)
-for i in List1:
-    if i == 46:
-        List1.remove(i)
-for i in List1:
-    if i == 85:
-        List1.remove(i)
-        print(List1)
-## Item 2
-## 2. Write this in one line of Python (you do not need to append to a list just print the output).
-## Attempt 1
-# List1 = [1, 2, 3, 6, 8, 12, 20, 32, 46, 85]
-# for elem in List1:
-#     if elem < 5:
-#         print(elem)
-## Attempt 2
-List1 = [1, 2, 3, 6, 8, 12, 20, 32, 46, 85]
 List2 =[]
 for elem in List1:
-    print(elem)
+    # print(elem)
     if elem < 5:
-        print(elem)
+        # print(elem)
         List2.append(elem)
-        # for elem in List1:
-        #     if elem > 5:
-        #         List1.remove(elem)
-        #         for elem in List1:
-        #             if elem > 5:
-        #                 List1.remove(elem)
 print(List2)
-## Success for both items? Yes for 1. No for 2.
+## Item 2
+## 2. Write this in one line of Python (you do not need to append to a list just print the output).
 
 List1 = [1, 2, 3, 6, 8, 12, 20, 32, 46, 85]
 print([element for element in List1 if element < 5])
