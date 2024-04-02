3218095e704969a762bdca11ae50cd2dc3833872
brendanpeckp <157844873+brendanpeckp@users.noreply.github.com>
2024-04-02 16:58:37 -0400

    backup

--- pythonProject/Challenges/challenge_08/singlepart_to_multipart_function.py
+++ pythonProject/Challenges/challenge_08/singlepart_to_multipart_function.py
@@ -9,14 +9,13 @@ arcpy.env.workspace = workspace
 def singlepart_to_multipart(in_features):
     '''This function takes singlepart line shapefiles and uses the ESRI Dissolve tool to convert it to a single
     multipart feature. It also describes the input and out put data.'''
-    describe_in_features = arcpy.Describe(os.path.join("gem_active_faults_harmonized.shp"))
+    # describe_in_features = arcpy.Describe(os.path.join("gem_active_faults_harmonized.shp"))
+    describe_in_features = arcpy.Describe(in_features)
     print("The input data, " + str(describe_in_features.name) + " is a " + str(describe_in_features.dataType))
-    out_feature_class = os.path.join("faults_dissolved.shp")
+    out_feature_class = os.path.join("multipart_line.shp")
     arcpy.management.Dissolve(in_features, out_feature_class, "", "", "MULTI_PART", "", "")
-    describe_out_feature_class = arcpy.Describe(os.path.join("faults_dissolved"))
+    describe_out_feature_class = arcpy.Describe(out_feature_class)
     print("The output data, " + str(describe_out_feature_class.name) + " is a " + str(describe_out_feature_class.dataType))
 
-# in_features = os.path.join("gem_active_faults_harmonized.shp")
-# singlepart_to_multipart(in_features)
-
+# This line uses the function to dissolve the active fault database.
 singlepart_to_multipart(os.path.join("gem_active_faults_harmonized.shp"))
